21aaaec2fd1ac166e34e9d4e60bb12e2ebdf7e53
Reed Ceniviva <44185694+RalexcenTemple@users.noreply.github.com>
2020-01-29 15:35:54 -0500

    wzip
    finished wzip.c file with comments

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -9,7 +9,7 @@
 
 
 int main(int args, char** argv){
-        if(args == 1){
+        if(args == 1){//incorrect number of arguments
                 puts("wzip: file1 [file2 ...]");
                 return 1;
         }
@@ -19,8 +19,8 @@ int main(int args, char** argv){
         int i = 1;
         while(i < args){
 		FILE *fp = fopen(argv[i], "r");
-                if(fp){
-                        while(1){
+                if(fp){//if opening the file worked
+                        while(1){//infinite while loop to handle multiple lines
 				c = fgetc(fp);
                                 if(count == 0){//catch initial run
                                         d = c;
@@ -28,9 +28,9 @@ int main(int args, char** argv){
                                 }else if(c == d){//adjust for conecutive matches
                                         count ++;
                                 }else{//adjust for a change in character
-                                        if(c==EOF){
+                                        if(c==EOF){//break out of while if its the end of the file
 						break;
-					}else{
+					}else{//write out the int and char, reassign the current char, reset count
 						fwrite(&count, sizeof(int), 1, stdout);
                                         	fwrite(&d, sizeof(char), 1, stdout);
                                         	d = c;
@@ -43,7 +43,7 @@ int main(int args, char** argv){
                         puts("FAILED TO OPEN FILE");
                         return(-1);
                 }
-                if(i == args){
+                if(i == args){//print the last character being counted and its count
                         fwrite(&count, sizeof(int), 1, stdout);
                         fwrite(&d, sizeof(char), 1, stdout);
                 }
