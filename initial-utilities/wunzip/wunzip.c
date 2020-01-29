34fedbc2e82fe832f9156f72e2e7cc7acd25fb4b
Reed Ceniviva <44185694+RalexcenTemple@users.noreply.github.com>
2020-01-29 15:35:19 -0500

    wunzip
    finished wunzip.c file with comments

--- initial-utilities/wunzip/wunzip.c
+++ initial-utilities/wunzip/wunzip.c
@@ -9,35 +9,30 @@
 
 
 int main(int args, char** argv){//got the main arguments mized up
-	if(args == 1){
+	if(args == 1){//if there arent enough arguments
 		printf("wunzip: file1 [file2 ...]\n");
 		return(1);
 	}
-	for(int i = 1; i < args; i++){
-		//printf("I: %d\n", i);
+	for(int i = 1; i < args; i++){// for each file passed 
 		FILE *fp = fopen(argv[i], "rb");
-		if(fp){
+		if(fp){//if the file is opened
 			char c;
 			int a;
-			//int j = 0;
 			while(1){
-				fread(&a, sizeof(int), 1, fp);
-				fread(&c, sizeof(char), 1, fp);
-				if(feof(fp)){
+				fread(&a, sizeof(int), 1, fp);//read in the number 
+				fread(&c, sizeof(char), 1, fp);//read in the char
+				if(feof(fp)){//break if the end of the file has been reached
 					break;
 				}
-				//printf("A: %d\n C: %c C(int): %d\n", a,c,(int)c);
-				//if(a == 1 || (int)c == 10){
-				if((int)c == 10){
+				if((int)c == 10){//if the char is the new line char then print a new line
 					puts("");	
 				}else{
-					for(int m = 0; m < a; m++){
+					for(int m = 0; m < a; m++){//print the char the number of times read to a (the count)
 						printf("%c",c);
 					}
 				}
-			//printf("J: %d\n", j);
 			}
-			fclose(fp);
+			fclose(fp);//close the file
 		}
 	}
 }
